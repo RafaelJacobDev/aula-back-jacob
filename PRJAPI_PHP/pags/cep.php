b194e1da673ab95edf8a9a97d0743fbeb783a691
RAFAEL JACOB TEIXEIRA GRANGEIRO <rafael.jtgrangeiro@senacsp.edu.br>
2025-09-22 17:17:51 -0300

    finalizando uso de API

--- PRJAPI_PHP/pags/cep.php
+++ PRJAPI_PHP/pags/cep.php
@@ -18,3 +18,22 @@ if (!isset($cep) || strlen($cep) != 8) {
     $context = stream_context_create($options);
     $response = file_get_contents($url, false, $context);
 }
+
+if ($response == false) {
+    $mensagem = "Erro ao acessar a API ViaCEP." ;
+    } else{
+        $dados= json_decode($response, true);
+        if (isset($dados['erro'])) {
+            $mensagem = "CEP não encontrado.";
+        } else {
+            echo "<h2> Endereço encontrado </h2>";
+            echo "<input type='text' value='{$dados['logradouro']}' disabled> <br>";
+            echo "<input type='text' value='{$dados['complemento']}'disabled> <br>";
+            echo "<input type='text' value='{$dados['bairro']}'disabled> <br>";
+            echo "<input type='text' value='{$dados['localidade']}'disabled> <br>";
+            echo "<input type='text' value='{$dados['estado']}'disabled> <br>";
+        }
+    }
+
+    echo "<p>{$mensagem}</P>";
+?>
