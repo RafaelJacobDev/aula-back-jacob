e3f2142bd0edf9d68bed0d3e0a35a06ef9a14f53
RAFAEL JACOB TEIXEIRA GRANGEIRO <rafael.jtgrangeiro@senacsp.edu.br>
2025-09-23 17:24:01 -0300

    finalização do primeiro uso de API

--- PRJAPI_PHP/pags/cep.php
+++ PRJAPI_PHP/pags/cep.php
@@ -25,15 +25,50 @@ if ($response == false) {
         $dados= json_decode($response, true);
         if (isset($dados['erro'])) {
             $mensagem = "CEP não encontrado.";
-        } else {
-            echo "<h2> Endereço encontrado </h2>";
-            echo "<input type='text' value='{$dados['logradouro']}' disabled> <br>";
-            echo "<input type='text' value='{$dados['complemento']}'disabled> <br>";
-            echo "<input type='text' value='{$dados['bairro']}'disabled> <br>";
-            echo "<input type='text' value='{$dados['localidade']}'disabled> <br>";
-            echo "<input type='text' value='{$dados['estado']}'disabled> <br>";
-        }
+        } //else {
+           // echo "<h2> Endereço encontrado </h2>";
+           // echo "<input type='text' value='{$dados['logradouro']}' disabled> <br>";
+           //echo "<input type='text' value='{$dados['complemento']}'disabled> <br>";
+           // echo "<input type='text' value='{$dados['bairro']}'disabled> <br>";
+           // echo "<input type='text' value='{$dados['localidade']}'disabled> <br>";
+           //echo "<input type='text' value='{$dados['estado']}'disabled> <br>";
+        
     }
 
-    echo "<p>{$mensagem}</P>";
+    //echo "<p>{$mensagem}</P>";
 ?>
+
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado - Busca CEP</title>
+     <link rel="stylesheet" href=".././css/estilo.css">
+</head>
+<body>
+    <div id="cep-buscado">
+        <div>
+            <label>Logradouro</label>
+            <input type="text" value="<?=isset($dados['logradouro']) ? $dados['logradouro'] : $mensagem ?>"disabled>
+        </div>
+        <div>
+            <label>Complemento</label>
+            <input type="text" value="<?=isset($dados['complemento']) ? $dados['complemento'] : $mensagem ?>"disabled>
+        </div>
+        <div>
+            <label>Bairro</label>
+            <input type="text" value="<?=isset($dados['bairro']) ? $dados['bairro'] : $mensagem ?>"disabled>
+        </div>
+        <div>
+            <label>Localidade</label>
+            <input type="text" value="<?=isset($dados['localidade']) ? $dados['localidade'] : $mensagem?>"disabled>
+        </div>
+        <div>
+            <label>Estado</label>
+            <input type="text" value="<?=isset($dados['estado']) ? $dados['estado'] : $mensagem?>"disabled>
+        </div>
+
+    </div>
+</body>
+</html>
